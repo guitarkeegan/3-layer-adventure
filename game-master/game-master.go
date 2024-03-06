da67b04090cf158202455f7099ee20bc30758f0f
guitarkeegan <keegananglim@gmail.com>
2024-03-06 12:03:02 -0800

    working on ai functions

--- game-master/game-master.go
+++ game-master/game-master.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/sashabaranov/go-openai"
+	"github.com/sashabaranov/go-openai/jsonschema"
 )
 
 func Play() {
@@ -33,18 +34,55 @@ func Play() {
 
 	player := NewPlayer(answers[0], answers[1], answers[2])
 
+	winParams := jsonschema.Definition{
+		Type: jsonschema.Object,
+		Properties: map[string]jsonschema.Definition{
+			"winningPrompt": {
+				Type:        jsonschema.String,
+				Description: "Use a prompt that will generate a prize image for the victorious user. It should relate to the things they love.",
+			},
+		},
+		Required: []string{"winningPrompt"},
+	}
+
+	loseParams := jsonschema.Definition{
+		Type: jsonschema.Object,
+		Properties: map[string]jsonschema.Definition{
+			"losingPrompt": {
+				Type:        jsonschema.String,
+				Description: "This prompt will be used to generate an image that the user will see upon losing the game. It should likely contain something related to what they fear.",
+			},
+		},
+		Required: []string{"losingPrompt"},
+	}
+
+	winFunc := openai.FunctionDefinition{
+		Name:        "win",
+		Description: "generate an image for the winner of the game, based on what they love",
+		Parameters:  winParams,
+	}
+	loseFunc := openai.FunctionDefinition{
+		Name:        "lose",
+		Description: "get an image url for the loser of the game, based on what they fear",
+		Parameters:  loseParams,
+	}
+
 	req := openai.ChatCompletionRequest{
 		Model: openai.GPT3Dot5Turbo,
+		Tools: []openai.Tool{
+			{Type: openai.ToolTypeFunction, Function: &winFunc},
+			{Type: openai.ToolTypeFunction, Function: &loseFunc},
+		},
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleSystem,
-				Content: fmt.Sprintf("You are a the game master. You are talking with the player of the game. Your job is to create a choose your own adventure, coupled with a specific musical challenge at each step of the journey. Create the adventure based on the fears and loves of the player. The musical challenges should be challenging, and should be centered around music theory. Example question: How do you spell a G major chord? Example: Spell the notes of an A major scale. It should also be noted that this is a text based game, so the player would need to be able to answer the questions without any audio. You can determine if the player has answered the question correctly or not. If you determine that the player answered incorrectly, you may end the game. If the player successfully solves 3 musical puzzels, they win the game. Try to weave in the musical challenges to the plot of the story. The player should be asked alternative questions about which direction they'd like to go, or which action they'd like to take, then be asked the musical challenges based on their action or choice of direction. Player name: %s, loves: %s, fears: %s ", player.Name, player.Loves, player.Fears),
+				Content: fmt.Sprintf("You are a the game master. You are talking with the player of the game. Your job is to create a choose your own adventure, coupled with a specific musical challenge at each step of the journey. Create the adventure based on the fears and loves of the player. The musical challenges should be challenging, and should be centered around music theory. Example question: How do you spell a G major chord? Example: Spell the notes of an A major scale. It should also be noted that this is a text based game, so the player would need to be able to answer the questions without any audio. You can determine if the player has answered the question correctly or not. If you determine that the player answered incorrectly, you may end the game. If the player successfully solves 3 musical puzzels, they win the game. If they answer any one musical question incorrectly, they lose the game. Try to weave in the musical challenges to the plot of the story. The player should be asked alternative questions about which direction they'd like to go, or which action they'd like to take, then be asked the musical challenges based on their action or choice of direction. Player name: %s, loves: %s, fears: %s ", player.Name, player.Loves, player.Fears),
 			},
 		},
 	}
 
 	fmt.Println("Musical Adventure")
-	fmt.Println("Great the gamemaster to get started")
+	fmt.Println("Greet the gamemaster to get started")
 	fmt.Println("---------------------\n")
 	fmt.Print("> ")
 	for s.Scan() {
@@ -58,6 +96,9 @@ func Play() {
 			continue
 		}
 		fmt.Printf("%s\n\n", resp.Choices[0].Message.Content)
+		if resp.Choices[0].Message.FunctionCall != nil {
+			fmt.Printf("---functionCallName: %s ---\n", resp.Choices[0].Message.FunctionCall.Name)
+		}
 		req.Messages = append(req.Messages, resp.Choices[0].Message)
 		fmt.Print("> ")
 	}
--- game-master/helper-funcs.go
+++ game-master/helper-funcs.go
@@ -1,25 +1,25 @@
 package gamemaster
 
 import (
-	"math/rand"
+	"context"
+	"fmt"
+
+	"github.com/sashabaranov/go-openai"
 )
 
-// Generates a random musical key for a scale or chord and returns its name
-func getRandomScaleOrChord() string {
-	// Define slices of musical keys and scale/chord types
-	keys := []string{"C", "D", "E", "F", "G", "A", "B", "C#", "D#", "F#", "G#", "A#"}
-	scales := []string{"Major", "Melodic Minor", "Harmonic Minor", "Natural Minor", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian"}
-	// Alternatively, for chords, you could use something like:
-	chords := []string{"Maj7", "min7", "7", "min7b5", "dim7", "Maj9", "min9", "9"}
+func win(client *openai.Client, prompt string) {
 
-	// Randomly select an element from each slice
-	key := keys[rand.Intn(len(keys))]
-	scale := scales[rand.Intn(len(scales))]
-	chord := chords[rand.Intn(len(chords))]
-
-	// Return the concatenated string
-	if rand.Intn(2)%2 == 0 {
-		return key + " " + scale
+	respUrl, err := client.CreateImage(
+		context.Background(),
+		openai.ImageRequest{
+			Prompt: prompt, Size: openai.CreateImageSize1024x1024,
+			ResponseFormat: openai.CreateImageResponseFormatURL,
+			N:              1,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
 	}
-	return key + " " + chord
+	fmt.Println(respUrl.Data[0].URL)
 }
