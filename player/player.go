6a2675436abbf80bf1300082539572ea82747dc9
guitarkeegan <keegananglim@gmail.com>
2024-03-08 22:29:49 -0800

    add player type and getplayerinfo method

--- player/player.go
+++ player/player.go
@@ -1,1 +1,35 @@
+// The player package is used to set and get player data
 package player
+
+import (
+	"errors"
+	"fmt"
+)
+
+// The PlayerFieldLimit is given as a security measure for now. It should probably
+// be replaced with a better solution.
+const (
+	PlayerFieldLimit = 50
+)
+
+// PlayerInfo holds the player information
+type PlayerInfo struct {
+	name  string
+	loves string
+	fears string
+}
+
+func Make(name, loves, fears string) (PlayerInfo, error) {
+	if len(name) > PlayerFieldLimit || len(loves) > PlayerFieldLimit || len(fears) > PlayerFieldLimit {
+		return PlayerInfo{}, errors.New(fmt.Sprintf("Player inputs are too long. Only %d characters are allowed for each field", PlayerFieldLimit))
+	}
+	return PlayerInfo{
+		name:  name,
+		loves: loves,
+		fears: fears,
+	}, nil
+}
+
+func (p PlayerInfo) GetPlayerInfo() (PlayerInfo, error) {
+	return p, nil
+}
